4153803d55cc1f97e86d1bafb382116d4d42e5c5
seriguchi_keigo <2364138keigo@gmail.com>
2026-03-30 23:52:37 +0900

    型を修正

--- src/utils/supabaseFunctions.ts
+++ src/utils/supabaseFunctions.ts
@@ -1,14 +1,15 @@
 import { supabase } from "./supabase";
+import { StudyRecord } from "../types/recordType";
 
 export const getAllRecords = async () => {
   const records = await supabase.from("study-record").select("*");
   return records;
 };
 
-export const insertRecord = async (record) => {
+export const insertRecord = async (record: StudyRecord) => {
   await supabase.from("study-record").insert(record);
 };
 
-export const deleteRecord = async (id) => {
+export const deleteRecord = async (id: string) => {
   await supabase.from("study-record").delete().eq("id", id);
 };
