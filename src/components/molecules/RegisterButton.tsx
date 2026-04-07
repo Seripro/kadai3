42beebe77d3285fed16f42827a2aa2e205dd6290
seriguchi_keigo <2364138keigo@gmail.com>
2026-04-07 18:54:03 +0900

    登録ボタンをコンポーネントに切り出した

---
+++ src/components/molecules/RegisterButton.tsx
@@ -0,0 +1,17 @@
+type Props = {
+  title: string;
+  time: number | '';
+  setError: React.Dispatch<React.SetStateAction<string>>;
+  createRecord: (title: string, time: number) => Promise<void>;
+};
+
+export const RegisterButton = (props: Props) => {
+  const handleClick = () => {
+    if (props.title === '' || props.time === 0 || props.time === '') {
+      props.setError('入力されていない項目があります');
+    } else {
+      props.createRecord(props.title, props.time);
+    }
+  };
+  return <button onClick={handleClick}>登録</button>;
+};
