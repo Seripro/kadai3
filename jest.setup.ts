648ec94686896853326761cbbda541e71291ef10
seriguchi_keigo <2364138keigo@gmail.com>
2026-04-24 00:22:08 +0900

    jest.setup.tsの更新: matchMediaとstructuredCloneのモックを追加

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,4 +1,27 @@
-import "@testing-library/jest-dom";
-import { config } from "dotenv";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import '@testing-library/jest-dom';
+import { config } from 'dotenv';
 
 config();
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+if (!global.structuredClone) {
+  global.structuredClone = function structuredClone(objectToClone: any) {
+    if (objectToClone === undefined) return undefined;
+    return JSON.parse(JSON.stringify(objectToClone));
+  };
+}
