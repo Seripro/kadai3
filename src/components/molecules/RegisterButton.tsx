2c073f6e66f0711d9e765b5b25b82ca398d992b7
seriguchi_keigo <2364138keigo@gmail.com>
2026-04-24 08:57:27 +0900

    0以下の数字を入力不可にし、学習内容と学習時間でエラーメッセージを分けた

--- src/components/molecules/RegisterButton.tsx
+++ src/components/molecules/RegisterButton.tsx
@@ -3,15 +3,27 @@ import { Button } from '@chakra-ui/react';
 type Props = {
   title: string;
   time: number | '';
+  error: string;
   setError: React.Dispatch<React.SetStateAction<string>>;
   createRecord: (title: string, time: number) => Promise<void>;
 };
 
 export const RegisterButton = (props: Props) => {
+  const errors = ['学習内容を入力してください', '正しい学習時間を入力してください'];
+  const titleCondition = props.title === '';
+  const timeCondition = props.time === 0 || props.time === '' || props.time <= 0;
   const handleClick = () => {
-    if (props.title === '' || props.time === 0 || props.time === '') {
-      props.setError('入力されていない項目があります');
-    } else {
+    props.setError('');
+    if (titleCondition) {
+      props.setError(errors[0]);
+    }
+    if (timeCondition) {
+      props.setError(errors[1]);
+    }
+    if (titleCondition && timeCondition) {
+      props.setError(`${errors[0]}\n${errors[1]}`);
+    }
+    if (!(titleCondition || timeCondition) && typeof props.time === 'number') {
       props.createRecord(props.title, props.time);
     }
   };
