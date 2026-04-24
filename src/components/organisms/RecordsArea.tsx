a3d3fc0d983e33c080b9dac3c900f5196b9cdeca
seriguchi_keigo <2364138keigo@gmail.com>
2026-04-24 20:14:27 +0900

    RecordsAreaコンポーネントにonUpdateプロパティを追加し、RecordAreaに更新機能を統合

--- src/components/organisms/RecordsArea.tsx
+++ src/components/organisms/RecordsArea.tsx
@@ -5,14 +5,17 @@ import { RecordArea } from '../molecules/RecordArea';
 type Props = {
   records: Record[];
   onDelete: (id: string, index: number) => void;
+  onUpdate: (id: string, title: string, time: number | null) => Promise<boolean>;
 };
 
 export const RecordsArea = (props: Props) => {
-  const { records, onDelete } = props;
+  const { records, onDelete, onUpdate } = props;
   return (
     <VStack>
       {records.map((record, index) => {
-        return <RecordArea key={index} id={record.id} title={record.title} time={record.time} index={index} onDelete={onDelete} />;
+        return (
+          <RecordArea key={index} id={record.id} title={record.title} time={record.time} index={index} onDelete={onDelete} onUpdate={onUpdate} />
+        );
       })}
     </VStack>
   );
