67d0577dd61a4ab776867d9798335dfbd9f6fd9b
seriguchi_keigo <2364138keigo@gmail.com>
2026-04-24 20:10:38 +0900

    update関数を定義

--- src/utils/supabaseFunctions.ts
+++ src/utils/supabaseFunctions.ts
@@ -1,15 +1,19 @@
-import { supabase } from "./supabase";
-import { InsertRecord } from "../types/recordType";
+import { supabase } from './supabase';
+import { InsertRecord, UpdateRecord } from '../types/recordType';
 
 export const getAllRecords = async () => {
-  const records = await supabase.from("study-record").select("*");
+  const records = await supabase.from('study-record').select('*');
   return records;
 };
 
 export const insertRecord = async (record: InsertRecord) => {
-  await supabase.from("study-record").insert(record);
+  await supabase.from('study-record').insert(record);
 };
 
 export const deleteRecord = async (id: string) => {
-  await supabase.from("study-record").delete().eq("id", id);
+  await supabase.from('study-record').delete().eq('id', id);
+};
+
+export const updateRecord = async (record: UpdateRecord) => {
+  await supabase.from('study-record').update(record).eq('id', record.id);
 };
