bef57e584aa4da5813382f887c9e0e89c87e87a8
seriguchi_keigo <2364138keigo@gmail.com>
2026-04-24 20:11:00 +0900

    update用のrecordの型を定義

--- src/types/recordType.ts
+++ src/types/recordType.ts
@@ -1,4 +1,10 @@
-import { Database } from "../../database.types";
+import { Database } from '../../database.types';
 
-export type StudyRecord = Database["public"]["Tables"]["study-record"]["Row"]
-export type InsertRecord = Database["public"]["Tables"]["study-record"]["Insert"]
+export type StudyRecord = Database['public']['Tables']['study-record']['Row'];
+export type InsertRecord = Database['public']['Tables']['study-record']['Insert'];
+export type UpdateRecord = {
+  created_at?: string | undefined;
+  id: string;
+  time?: number | null;
+  title?: string | undefined;
+};
