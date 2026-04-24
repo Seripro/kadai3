6668d168e579fb321402c733012c1e80bedf1f60
seriguchi_keigo <2364138keigo@gmail.com>
2026-04-24 20:14:13 +0900

    RecordAreaコンポーネントにEditModalを追加し、更新機能を統合

--- src/components/molecules/RecordArea.tsx
+++ src/components/molecules/RecordArea.tsx
@@ -1,6 +1,7 @@
 import { HStack } from '@chakra-ui/react';
 import { DeleteButton } from './DeleteButton';
 import { TextCard } from './TextCard';
+import { EditModal } from '../organisms/EditModal';
 
 type Props = {
   id: string;
@@ -8,6 +9,7 @@ type Props = {
   time: number | null;
   index: number;
   onDelete: (id: string, index: number) => void;
+  onUpdate: (id: string, title: string, time: number | null) => Promise<boolean>;
 };
 
 export const RecordArea = (props: Props) => {
@@ -17,6 +19,7 @@ export const RecordArea = (props: Props) => {
         <TextCard>{props.title}</TextCard>
         <TextCard>{props.time}時間</TextCard>
         <DeleteButton id={props.id} index={props.index} onDelete={props.onDelete} />
+        <EditModal id={props.id} title={props.title} time={props.time} onUpdate={props.onUpdate} />
       </HStack>
     </div>
   );
